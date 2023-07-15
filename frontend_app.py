b36ef7e23458ec18e1969fb0375411b664401953
Shahriar <Jambaloo@mitras-MacBook-Pro.fritz.box>
2023-07-15 02:21:14 +0200

    Implements routes for navbar menu

--- frontend_app.py
+++ frontend_app.py
@@ -8,5 +8,30 @@ def home():
     return render_template("index.html")
 
 
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template("about.html")
+
+
+@app.route('/sidebar_left', methods=['GET'])
+def sidebar_left():
+    return render_template("sidebar-left.html")
+
+
+@app.route('/sidebar_right', methods=['GET'])
+def sidebar_right():
+    return render_template("sidebar-right.html")
+
+
+@app.route('/single', methods=['GET'])
+def single():
+    return render_template("single.html")
+
+
+@app.route('/blog', methods=['GET'])
+def blog():
+    return render_template("blog.html")
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
